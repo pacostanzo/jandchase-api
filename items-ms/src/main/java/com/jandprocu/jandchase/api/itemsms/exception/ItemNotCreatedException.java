5a314a4f32e1efa093ed2145e7c17ca1a9914bf2
Pablo Costanzo <costanzopa@gmail.com>
2019-08-23 01:32:50 -0300

    Delete Items Ms

--- items-ms/src/main/java/com/jandprocu/jandchase/api/itemsms/exception/ItemNotCreatedException.java
+++
@@ -1,7 +0,0 @@
-package com.jandprocu.jandchase.api.itemsms.exception;
-
-public class ItemNotCreatedException extends RuntimeException {
-    public ItemNotCreatedException(String message) {
-        super(message);
-    }
-}
