85785005d25445b6ad703d5e33266d6e46aae1fa
costanzopa <costanzopa@gmail.com>
2019-11-03 23:33:50 -0300

    Adding load balancing for oauth client.

--- oauth-ms/src/main/java/com/jandprocu/janchase/api/oauthms/OauthMsApplication.java
+++ oauth-ms/src/main/java/com/jandprocu/janchase/api/oauthms/OauthMsApplication.java
@@ -1,13 +1,16 @@
 package com.jandprocu.janchase.api.oauthms;
 
+import com.jandprocu.janchase.api.oauthms.config.RibbonConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
+@RibbonClient(name = "server", configuration = RibbonConfiguration.class)
 public class OauthMsApplication {
 
     public static void main(String[] args) {
